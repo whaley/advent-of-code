7cfdea18eac19d3a0769ebce2c294f4e4bcdcd27
Jason Whaley <jasonwhaley@gmail.com>
2018-01-07 11:52:27 -0500

    Completed Day One : Part One

--- src/main/kotlin/com/morninghacks/aoc2017/main.kt
+++
@@ -1,6 +0,0 @@
-package com.morninghacks.aoc2017
-
-fun main(args: Array<String>) {
-    println("Hello World")
-}
-
---
+++ src/test/kotlin/com/morninghacks/aoc2017/Day01Spec.kt
@@ -0,0 +1,53 @@
+package com.morninghacks.aoc2017
+
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.given
+import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.api.dsl.on
+import kotlin.test.assertEquals
+
+
+object Day01Spec : Spek({
+    given("Day One : Part One") {
+        val dataProvider = mapOf("1122" to 3,
+                "1111" to 4,
+                "1234" to 0,
+                "91212129" to 9)
+
+
+        on("captcha") {
+            for ((input, expected) in dataProvider) {
+                it("should produce $expected") {
+                    assertEquals(expected, captcha(input))
+                }
+            }
+        }
+
+        on("functionalCaptcha") {
+            for ((input, expected) in dataProvider) {
+                it("should produce $expected") {
+                    assertEquals(expected, functionalCaptcha(input))
+                }
+            }
+        }
+    }
+
+    given("Day One : Part Two") {
+        val dataProvider = mapOf("1212" to 6,
+                "1221" to 0,
+                "123425" to 4,
+                "123123" to 12,
+                "12131415" to 4)
+
+        on("captcha") {
+            for ((input, expected) in dataProvider) {
+                it("should produce $expected") {
+                    assertEquals(expected, captcha(input))
+                }
+            }
+        }
+    }
+})
+
+
+
