bd2b9998f33c12a8ae21711699a425a6e32702d2
Jason Whaley <jasonwhaley@gmail.com>
2018-07-22 12:10:15 -0400

    Calculate Day05 Part 1 answer with actual input

--- src/main/kotlin/com/morninghacks/aoc2017/Day05.kt
+++ src/main/kotlin/com/morninghacks/aoc2017/Day05.kt
@@ -1,5 +1,7 @@
 package com.morninghacks.aoc2017
 
+import java.lang.invoke.MethodHandles
+
 tailrec fun calculateNumberOfJumps(jumpList: List<Int>, position: Int = 0, step: Int = 0): Int {
     if (position >= jumpList.size) {
         return step
@@ -7,4 +9,10 @@ tailrec fun calculateNumberOfJumps(jumpList: List<Int>, position: Int = 0, step:
     val newPos = jumpList[position] + position
     val updatedList = jumpList.mapIndexed {idx,value -> if (idx == position) value + 1 else value}
     return calculateNumberOfJumps(updatedList, newPos, step + 1)
+}
+
+fun main(args: Array<String>) {
+    val jumpList = MethodHandles.lookup().lookupClass().getResourceAsStream("/Day05Input.txt")
+            .bufferedReader().lineSequence().map { s -> s.toInt() }.toList()
+    println(calculateNumberOfJumps(jumpList))
 }
