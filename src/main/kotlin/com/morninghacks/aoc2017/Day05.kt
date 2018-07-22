48c20c83ef883893eedf1bc53972b5d01afc2123
Jason Whaley <jasonwhaley@gmail.com>
2018-07-22 12:48:49 -0400

    Use a mutable list internally instead for Day05

--- src/main/kotlin/com/morninghacks/aoc2017/Day05.kt
+++ src/main/kotlin/com/morninghacks/aoc2017/Day05.kt
@@ -2,13 +2,18 @@ package com.morninghacks.aoc2017
 
 import java.lang.invoke.MethodHandles
 
-tailrec fun calculateNumberOfJumps(jumpList: List<Int>, postJumpAction : (Int) -> Int = ::incAction, position: Int = 0, step: Int = 0): Int {
-    if (position >= jumpList.size) {
-        return step
+fun calculateNumberOfJumps(jumpList: List<Int>, postJumpAction : (Int) -> Int = ::incAction, position: Int = 0, step: Int = 0): Int {
+    tailrec fun loop(jumpList: MutableList<Int>, position: Int = 0, step: Int = 0) : Int {
+        if (position >= jumpList.size) {
+            return step
+        }
+
+        val newPosition = position + jumpList[position]
+        jumpList[position] = postJumpAction(jumpList[position])
+        return loop(jumpList, newPosition, step + 1)
     }
-    val newPos = jumpList[position] + position
-    val updatedList = jumpList.mapIndexed {idx,value -> if (idx == position) postJumpAction(value) else value}
-    return calculateNumberOfJumps(updatedList, postJumpAction, newPos, step + 1)
+
+    return loop(jumpList.toMutableList())
 }
 
 private fun incAction(i: Int) : Int = i + 1
